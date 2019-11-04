7320ab9c54bf2bc8203da955f966d4a075fb0446
ObservedObserver <270001151@qq.com>
2019-11-04 21:42:09 +0800

    Revert "fix: csv on windows"
    This reverts commit 77580fb34d6bd5a2dfd516f6af9822993b2ca9bc.

--- packages/frontend/src/utils/fileParser.ts
+++ packages/frontend/src/utils/fileParser.ts
@@ -1,46 +1,25 @@
 import { Record, DataSource } from '../global';
 import Papa from 'papaparse';
-// import datalib from 'datalib';
-// import CSV from 'comma-separated-values';
+
 export function csvLoader (file: File) {
   return new Promise<DataSource>((resolve, reject) => {
-    let reader = new FileReader()
-    reader.readAsText(file)
-    reader.onload = (ev) => {
-      if (ev.target) {
-        try {
-          // todo: datalib can guess the tyoe
-          // const rawData: DataSource = datalib.read(ev.target.result as string, {type: 'csv', parse: 'auto'})
-
-          // const rawData: DataSource = new CSV(ev.target.result as string, { header: true}).parse()
-          let str = ev.target.result as string;
-          str.replace(/[\r\n]+$/g, '\n')
-          Papa.parse(ev.target.result as string, {
-            complete (results, file) {
-              let data: string[][] = results.data;
-              let fields: string[] = data[0];
-              let rawData = data.slice(1).map(row => {
-                let record: Record = {};
-                fields.forEach((field, index) => {
-                  record[field] = row[index]
-                })
-                return record
-              })
-              resolve(rawData)
-            },
-            error (error, file) {
-              reject(error)
-            }
-          });
-
-        } catch (error) {
-          reject(error)
-        }
-      } else {
-        reject(ev)
+    Papa.parse(file, {
+      complete (results, file) {
+        let data: string[][] = results.data;
+        let fields: string[] = data[0];
+        let rawData = data.slice(1).map(row => {
+          let record: Record = {};
+          fields.forEach((field, index) => {
+            record[field] = row[index]
+          })
+          return record
+        })
+        resolve(rawData)
+      },
+      error (error, file) {
+        reject(error)
       }
-    }
-    reader.onerror = reject
+    })
   })
 }
 
