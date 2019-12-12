488cf62043a386dac22e84c5660f168e25a5d25a
chenhao.haochen <chenhao.haochen@alibaba-inc.com>
2019-12-12 16:22:24 +0800

    12.12milestone

---
+++ precache-manifest.303f3a84bea3cbc815b0b660e76014ba.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6472f0f23230a3a6f0b94abe59190979",
+    "url": "/308422012c78c5a42163.worker.js"
+  },
+  {
+    "revision": "a657a08154f78677d019a0f10a6c6725",
+    "url": "/386bbcb7496e5493b6fb.worker.js"
+  },
+  {
+    "revision": "dbcbffcfbd4acbe9aa8d36aa8592970c",
+    "url": "/6c9c09ce4422e8e00e69.worker.js"
+  },
+  {
+    "revision": "c2f0831f5e63222738ea762f9c1b3594",
+    "url": "/a7fa28044adaf0bd915c.worker.js"
+  },
+  {
+    "revision": "962a6939c7d58b7eb09274c9c7ddca5d",
+    "url": "/ebfe4187a9bd4301e2e9.worker.js"
+  },
+  {
+    "revision": "7afe364c9bb7a06ec2ca92361a5ac62b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3d0a0699b86240c85686",
+    "url": "/static/css/2.d1461702.chunk.css"
+  },
+  {
+    "revision": "5ef9c6c8b5ea5abd80cd",
+    "url": "/static/css/main.351e600d.chunk.css"
+  },
+  {
+    "revision": "3d0a0699b86240c85686",
+    "url": "/static/js/2.baa7abc3.chunk.js"
+  },
+  {
+    "revision": "5ef9c6c8b5ea5abd80cd",
+    "url": "/static/js/main.62fee929.chunk.js"
+  },
+  {
+    "revision": "83c77388f0187cebf8da",
+    "url": "/static/js/runtime-main.e9878bd9.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.9892169006dba5422b92954f73c626e8.js"
+  "/precache-manifest.303f3a84bea3cbc815b0b660e76014ba.js"
 );
 
 self.addEventListener('message', (event) => {
