79d5c8bdc6c9e60f6adca19ea6155d5b1bf26ae9
ObservedObserver <270001151@qq.com>
2020-03-09 12:05:09 +0800

    more insights

---
+++ precache-manifest.20887f4f94995e8599d3d6420ac1222e.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "070c6e64ac7d2180366eef52a56749e1",
+    "url": "./073a512bec9d62518d2c.worker.js"
+  },
+  {
+    "revision": "442f77fda8ca1b590dd9805a9313cefa",
+    "url": "./aba8a4d8cd652cee6caf.worker.js"
+  },
+  {
+    "revision": "c2f61be0056173fa73aa1414d947b487",
+    "url": "./ae30e4247e69fb8a77a4.worker.js"
+  },
+  {
+    "revision": "0e85ad4c9cb54259f8fb622f3b829127",
+    "url": "./d19b89e3de65e9888604.worker.js"
+  },
+  {
+    "revision": "4ceeb3e0bf66db335dd6f74ead50bfd8",
+    "url": "./f3bc8a1633357a2c2611.worker.js"
+  },
+  {
+    "revision": "ea1e505a883095a26ae958dab9d168a4",
+    "url": "./f808b4c276283cfce6c8.worker.js"
+  },
+  {
+    "revision": "4381d557a2766241b26a0ed9c7b92e3f",
+    "url": "./index.html"
+  },
+  {
+    "revision": "ca8832d6241dad923914",
+    "url": "./static/css/2.d1461702.chunk.css"
+  },
+  {
+    "revision": "3f8a2082fe92551726cd",
+    "url": "./static/css/main.351e600d.chunk.css"
+  },
+  {
+    "revision": "ca8832d6241dad923914",
+    "url": "./static/js/2.0e14df54.chunk.js"
+  },
+  {
+    "revision": "ab76b1aefa545e07dabf746e0e8cf958",
+    "url": "./static/js/2.0e14df54.chunk.js.LICENSE"
+  },
+  {
+    "revision": "3f8a2082fe92551726cd",
+    "url": "./static/js/main.8f5258e0.chunk.js"
+  },
+  {
+    "revision": "64c46cccf223ee2b3944",
+    "url": "./static/js/runtime-main.cc17b21f.js"
+  },
+  {
+    "revision": "4a15bb11a192a28b32f61b30b13e5708",
+    "url": "./static/media/rath-glasses.4a15bb11.png"
+  },
+  {
+    "revision": "57f336a061365e63bf2380a44b116c60",
+    "url": "./static/media/rath.57f336a0.png"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/Rath/precache-manifest.99812d59985834b806bfdfe205c57af1.js"
+  "./precache-manifest.20887f4f94995e8599d3d6420ac1222e.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/Rath/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("./index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
