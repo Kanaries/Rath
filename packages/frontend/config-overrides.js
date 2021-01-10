919724023969dd3fba103c2ede10b27c73df2e5a
ObservedObserver <270001151@qq.com>
2021-01-10 21:58:54 +0800

    update: 依赖升级
    react 16 -> 17
    create-react-app
    worker-loader
    typescript
    ui libs

--- packages/frontend/config-overrides.js
+++ packages/frontend/config-overrides.js
@@ -2,7 +2,12 @@ module.exports = function override(config, env) {
   // do stuff with the webpack config...
   config.module.rules.push({
     test: /\.worker\.js$/,
-    use: { loader: 'worker-loader' }
+    use: {
+      loader: 'worker-loader',
+      options: {
+        inline: 'fallback',
+      },
+    },
   })
   config.output.globalObject = 'self'
   return config;
