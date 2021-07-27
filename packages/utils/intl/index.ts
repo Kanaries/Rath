7bf7f9e2b19e5786065a1bff4bc22609827b047b
Hao Chen <270001151@qq.com>
2021-07-27 15:25:07 +0800

    temp: intl替换
    react-universal-intl 在vite下存在问题，准备去除该模块的依赖

---
+++ packages/utils/intl/index.ts
@@ -0,0 +1,43 @@
+interface IObject {
+    [key: string]: any;
+}
+function deepFrom (obj: any, keys: string[], depth: number): any {
+    const currentKey = keys[depth];
+    const hasKey = currentKey in obj;
+    if (!(obj instanceof Object) && depth < keys.length - 1) {
+        return null;
+    }
+    if (obj instanceof Object && !(currentKey in obj)) {
+        return null;
+    }
+    if (depth === keys.length - 1) {
+        return obj[currentKey];
+    }
+    return deepFrom(obj[currentKey], keys, depth + 1);
+}
+
+class LangIntl {
+    private locales: { [key: string]: any } = {}
+    private currentLocale: string = 'zh-CN';
+    private deepGet (keyStr: string) {
+        const locale = this.locales[this.currentLocale];
+        if (keyStr in locale) {
+            const keys = keyStr.split('.');
+            const value = deepFrom(locale, keys, 0);
+            if (value === null) {
+                console.warn('kv不匹配');
+                return '';
+            }
+            return value;
+        }
+        console.warn('locale 资源未加载.')
+        return ''
+    }
+    public get (keyStr: string) {
+        return this.deepGet(keyStr)
+    }
+}
+function intl () {
+
+}
+export default intl;
