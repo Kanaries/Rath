17931f487ab4464705e81b901ca59788ffde0659
Hao Chen <270001151@qq.com>
2021-08-24 23:52:42 +0800

    fix: graphic walker 本地开发版调整

--- packages/graphic-walker/src/main.tsx
+++ packages/graphic-walker/src/main.tsx
@@ -1,11 +1,11 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import './index.css'
-import App from './App'
+import { GraphicWalker } from './index'
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <GraphicWalker />
   </React.StrictMode>,
   document.getElementById('root')
 )
