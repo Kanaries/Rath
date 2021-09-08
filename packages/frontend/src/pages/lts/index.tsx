87b61f260b91553e9b3994be6f6d6e3f401676ac
Hao Chen <270001151@qq.com>
2021-09-08 20:42:09 +0800

    fix: test effects without insight worker + ui adjust for lts

--- packages/frontend/src/pages/lts/index.tsx
+++ packages/frontend/src/pages/lts/index.tsx
@@ -1,43 +1,116 @@
+import { makeAutoObservable, observable, runInAction } from 'mobx';
 import { observer } from 'mobx-react-lite';
+import { DefaultButton, PrimaryButton, Toggle, Stack } from 'office-ui-fabric-react';
 import React, { useEffect, useState } from 'react';
+import { useRef } from 'react';
+import { ISpec } from 'visual-insights/build/esm/insights/InsightFlow/specification/encoding';
+import { IRow } from '../../interfaces';
 import { useGlobalStore } from '../../store';
+import { LTSPipeLine } from '../../store/pipeLineStore/lts';
 import BaseChart from '../../visBuilder/vegaBase';
+
+class PageTMPStore {
+    public pageIndex: number = 0;
+    public aggState: boolean = false;
+    private ltsPipeLineStore: LTSPipeLine;
+    public spec: { schema: ISpec; dataView: IRow[] } | undefined = undefined;
+    constructor (ltsPipeLineStore: LTSPipeLine) {
+        makeAutoObservable(this, {
+            spec: observable.ref
+        });
+        this.ltsPipeLineStore = ltsPipeLineStore;
+    }
+    public async emitViewChangeTransaction(index: number) {
+        // pipleLineStore统一提供校验逻辑
+        if (this.ltsPipeLineStore.insightSpaces && this.ltsPipeLineStore.insightSpaces.length > index) {
+            const spec = this.ltsPipeLineStore.specify(index);
+            if (spec) {
+                // this.spec = spec;
+                const agg = !spec.schema.geomType?.includes('point');
+                runInAction(() => {
+                    this.spec = spec;
+                    this.aggState = agg;
+                    this.pageIndex = index;
+                })
+            }
+        }
+    }
+    public setAggState (aggState: boolean) {
+        this.aggState = aggState;
+    }
+}
+
+function useTMPStore (ltsPipeLineStore: LTSPipeLine) {
+    const storeRef = useRef<PageTMPStore>(new PageTMPStore(ltsPipeLineStore));
+    return storeRef.current;
+}
 
 const LTSPage: React.FC = props => {
     const { ltsPipeLineStore, dataSourceStore } = useGlobalStore();
     const { insightSpaces } = ltsPipeLineStore;
-    const [pageIndex, setPageIndex] = useState(0);
-    const [isAgg, setIsAgg] = useState(true);
-    useEffect(() => {
-        ltsPipeLineStore.startTask();
-    }, [])
-    const spec = ltsPipeLineStore.specify(pageIndex);
+    const tmpStore = useTMPStore(ltsPipeLineStore);
+
+    if (tmpStore === null) return null;
+
+    const { pageIndex, aggState, spec } = tmpStore;
+
+
     return <div className="content-container">
-        <button onClick={() => {
-            setPageIndex(p => (p + 1) % insightSpaces.length)
-        }}>next</button>
-        <button onClick={() => {
-            setIsAgg(a => !a)
-        }}>agg: {Number(isAgg)}</button>
-        <h1>subspaces length: {insightSpaces.length}</h1>
-        <div>
-            view {pageIndex} {insightSpaces.length > 0 && insightSpaces[pageIndex].score}:
+        <div className="card">
+            <Stack horizontal>
+                <PrimaryButton
+                    text="Analysis"
+                    disabled={dataSourceStore.cleanedData.length === 0}
+                    onClick={() => {
+                        ltsPipeLineStore.startTask().then(() => {
+                            tmpStore.emitViewChangeTransaction(0)
+                        })
+                    }}
+                />
+                <DefaultButton
+                    style={{ marginLeft: "10px" }}
+                    text="←"
+                    onClick={() => {
+                        tmpStore.emitViewChangeTransaction((pageIndex - 1 + insightSpaces.length) % insightSpaces.length)
+                    }}
+                />
+                <DefaultButton
+                    style={{ marginLeft: "10px" }}
+                    text="→"
+                    onClick={() => {
+                        tmpStore.emitViewChangeTransaction((pageIndex + 1) % insightSpaces.length)
+                    }}
+                />
+            </Stack>
+            <Toggle
+                checked={aggState}
+                onText="On"
+                offText="Off"
+                label="Default Aggregate"
+                onChange={(e, checked) => {
+                    tmpStore.setAggState(Boolean(checked))
+                }}
+            />
+            <h2>Visual Insights(v2 engine β)</h2>
             <div>
-                {insightSpaces.length > 0 && spec && <div>
-                    <BaseChart
-                        defaultAggregated={isAgg}
-                        defaultStack={true}
-                        dimensions={insightSpaces[pageIndex].dimensions}
-                        measures={insightSpaces[pageIndex].measures}
-                        dataSource={dataSourceStore.cleanedData}
-                        schema={spec.schema}
-                        fieldFeatures={dataSourceStore.fieldMetas.map(f => ({
-                            name: f.fid,
-                            type: f.semanticType
-                        }))}
-                        aggregator="sum"
-                    />
-                </div>}
+                <p className="state-description">results: {pageIndex + 1} / {insightSpaces.length}. score: {insightSpaces.length > 0 && insightSpaces[pageIndex].score?.toFixed(2)}</p>
+                <div>
+                    {insightSpaces.length > 0 && spec && <div>
+                        <BaseChart
+                            defaultAggregated={aggState}
+                            defaultStack={true}
+                            dimensions={insightSpaces[pageIndex].dimensions}
+                            measures={insightSpaces[pageIndex].measures}
+                            dataSource={dataSourceStore.cleanedData}
+                            schema={spec.schema}
+                            fieldFeatures={dataSourceStore.fieldMetas.map(f => ({
+                                name: f.fid,
+                                type: f.semanticType
+                            }))}
+                            aggregator="sum"
+                        />
+                    </div>}
+                </div>
             </div>
         </div>
     </div>
--- packages/frontend/src/store/pipeLineStore/lts.ts
+++ packages/frontend/src/store/pipeLineStore/lts.ts
@@ -1,35 +1,93 @@
 import { makeAutoObservable, observable, runInAction } from "mobx";
-import { VIEngine } from "visual-insights/build/esm/insights";
+import { InsightWorker } from "visual-insights/build/esm/commonTypes";
+import { DefaultIWorker, VIEngine } from "visual-insights/build/esm/insights";
+import { ViewSpace } from "visual-insights/build/esm/insights/dev";
 import { IInsightSpace } from "visual-insights/build/esm/insights/InsightFlow/interfaces";
+// import { simpleAggregate } from "visual-insights/build/esm/statistics";
+import { IRow } from "../../interfaces";
 import { DataSourceStore } from "../dataSourceStore";
 
+const identityWorker: InsightWorker = async (aggData, dimensions, measures) => {
+    return {
+        dimensions,
+        measures,
+        significance: 1
+    }
+}
+
+class TestEngine extends VIEngine {
+    public async insightExtraction(viewSpaces: ViewSpace[] = this.subSpaces): Promise<IInsightSpace[]> {
+        const context = this;
+        let ansSpace: IInsightSpace[] = [];
+        for (let space of viewSpaces) {
+            const { dimensions, measures } = space;
+
+            let cube = context.cube;
+            let cuboid = cube.getCuboid(dimensions);
+            const aggData = cuboid.getState(measures, measures.map(() => 'sum'));
+
+            const imp = VIEngine.getSpaceImpurity(aggData, dimensions, measures);
+            ansSpace.push({
+                impurity: imp,
+                significance: 1,
+                dimensions,
+                measures
+            })
+        }
+        context.insightSpaces = ansSpace;
+        return ansSpace;
+    }
+}
 export class LTSPipeLine {
     private dataSourceStore: DataSourceStore;
-    private vie: VIEngine;
+    private vie: TestEngine;
+    // private vie: VIEngine;
     public insightSpaces: IInsightSpace[];
     constructor (dataSourceStore: DataSourceStore) {
         makeAutoObservable(this, {
             insightSpaces: observable.ref
         });
         this.dataSourceStore = dataSourceStore;
-        const vie = new VIEngine();
+        const vie = new TestEngine();
+        // const vie = new VIEngine();
+        vie.workerCollection.register('identity', identityWorker);
+        vie.workerCollection.enable(DefaultIWorker.outlier, false);
+        vie.workerCollection.enable(DefaultIWorker.trend, false);
         // vie.setDataSource(dataSourceStore.cleanedData)
         this.vie = vie;
         this.insightSpaces = [] as IInsightSpace[];
     }
     public async startTask () {
         const { cleanedData, dimensions, measures } = this.dataSourceStore;
+        // const times: number[] = [];
+        // const prints: IRow[] = [];
+        // times.push(performance.now())
         this.vie.setDataSource(cleanedData)
         .setFieldKeys([...dimensions, ...measures])
             .buildfieldsSummary()
             .setDimensions(dimensions)
             .setMeasures(measures)
-        this.vie.buildGraph()
-            .clusterFields()
-            .buildCube()
-            .buildSubspaces();
+        // times.push(performance.now())
+        // prints.push({ task: 'init&univar', value: times[times.length - 1] - times[times.length - 2] })
+        this.vie.buildGraph();
+        // times.push(performance.now())
+        // prints.push({ task: 'co-graph', value: times[times.length - 1] - times[times.length - 2] })
+        this.vie.clusterFields();
+        // times.push(performance.now())
+        // prints.push({ task: 'clusters', value: times[times.length - 1] - times[times.length - 2] })
+        this.vie.buildCube();
+        // times.push(performance.now())
+        // prints.push({ task: 'cube', value: times[times.length - 1] - times[times.length - 2] })
+        this.vie.buildSubspaces();
+        // times.push(performance.now())
+        // prints.push({ task: 'subspaces', value: times[times.length - 1] - times[times.length - 2] })
         await this.vie.insightExtraction();
+        // times.push(performance.now())
+        // prints.push({ task: 'insights', value: times[times.length - 1] - times[times.length - 2] })
         this.vie.setInsightScores();
+        // times.push(performance.now())
+        // prints.push({ task: 'scores', value: times[times.length - 1] - times[times.length - 2] })
+        // console.log(JSON.stringify(prints, null, 2))
         const _insightSpaces = this.vie.insightSpaces.filter(s => typeof s.score === 'number' && !isNaN(s.score));
         const insightSpaces: IInsightSpace[] = [];
         let keyset = new Set();
