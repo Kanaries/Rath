23201e45ba0087199022c76daf91e81ce03a7a06
Hao Chen <270001151@qq.com>
2021-09-23 10:30:19 +0800

    feat: tailwind config in graphic-walker

--- packages/graphic-walker/src/index.tsx
+++ packages/graphic-walker/src/index.tsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import App, { EditorProps } from './App';
 import { StoreWrapper } from './store/index';
+import "tailwindcss/tailwind.css"
+import './index.css'
 
 export const GraphicWalker: React.FC<EditorProps> = props => {
     return <StoreWrapper>
