e40c767b2e556d6e5da70ad389e9482427900b15
Hao Chen <270001151@qq.com>
2022-03-20 16:38:59 +0800

    fix: old interface

--- packages/graphic-walker/src/Fields/fieldsContext.tsx
+++ packages/graphic-walker/src/Fields/fieldsContext.tsx
@@ -1,5 +1,5 @@
 import React, { useCallback } from 'react';
-import { DraggableFieldState, Field, IDraggableStateKey } from '../interfaces';
+import { DraggableFieldState, IDraggableStateKey } from '../interfaces';
 import {
     DragDropContext,
     DropResult,
