3c8275cffc7277ed2dd79ca2a9a312fee16bd892
Hao Chen <270001151@qq.com>
2022-06-30 15:48:39 +0800

    chore: fix webworker file name conflict

--- packages/frontend/config-overrides.js
+++ packages/frontend/config-overrides.js
@@ -6,6 +6,7 @@ module.exports = function override(config, env) {
       loader: 'worker-loader',
       options: {
         inline: 'fallback',
+        filename: '[path].[name].[contenthash].worker.js'
       },
     },
   })
