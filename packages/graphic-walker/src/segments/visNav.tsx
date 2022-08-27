8767d4f8a6d7944a0f9c821f06a44037b3e4e878
observedobserver <270001151@qq.com>
2022-08-27 21:58:54 +0800

    feat: allow edit tab name

--- packages/graphic-walker/src/segments/visNav.tsx
+++ packages/graphic-walker/src/segments/visNav.tsx
@@ -1,34 +1,44 @@
-import React from "react";
+import React, { useCallback } from "react";
 import { observer } from "mobx-react-lite";
-import PureTabs from "../components/tabs/pureTab";
+import PureTabs, { ITabOption } from "../components/tabs/pureTab";
 import { useGlobalStore } from "../store";
+
+const ADD_KEY = '_add';
+
 const VisNav: React.FC = (props) => {
     const { vizStore, commonStore } = useGlobalStore();
     const { visIndex, visList } = vizStore;
     const { currentDataset } = commonStore;
-    const tabs = visList.map((v) => ({
+    const tabs: ITabOption[] = visList.map((v) => ({
         key: v.visId,
         label: v.name || "vis",
     }))
     tabs.push({
-        key: '_add',
-        label: 'Add'
+        key: ADD_KEY,
+        label: '+ New'
     })
+
+    const visSelectionHandler = useCallback((tabKey: string, tabIndex: number) => {
+        if (tabKey === ADD_KEY) {
+            vizStore.saveVisChange();
+            vizStore.addVisualization();
+            vizStore.initMetaState(currentDataset)
+        } else {
+            vizStore.saveVisChange();
+            vizStore.selectVisualization(tabIndex);
+        }
+    }, [currentDataset, vizStore])
+
+    const editLabelHandler = useCallback((content: string, tabIndex: number) => {
+        vizStore.setVisName(tabIndex, content)
+    }, [])
+
     return (
         <PureTabs
             selectedKey={visList[visIndex].visId}
             tabs={tabs}
-            onSelected={(sk, index) => {
-                console.log(sk, index)
-                if (sk === '_add') {
-                    vizStore.saveVisChange();
-                    vizStore.addVisualization();
-                    vizStore.initMetaState(currentDataset)
-                } else {
-                    vizStore.saveVisChange();
-                    vizStore.selectVisualization(index);
-                }
-            }}
+            onEditLabel={editLabelHandler}
+            onSelected={visSelectionHandler}
         />
     );
 };
