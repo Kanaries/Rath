6f7cbc27b8a8473e2f12e8ed7ccc186e0c3dd1cf
kyusho <antoineyang99@gmail.com>
2022-09-14 14:58:00 +0800

    chore(g_walker): Use "serve" and "concurrently" to decouple and rewrite dev script.

--- packages/graphic-walker/src/dataSource/config.ts
+++ packages/graphic-walker/src/dataSource/config.ts
@@ -9,15 +9,15 @@ export const DemoDataAssets = process.env.NODE_ENV === 'production' ? {
     KELPER: 'https://chspace.oss-cn-hongkong.aliyuncs.com/api/ds-kelper-service.json',
 } : {
     // CARS: "https://chspace.oss-cn-hongkong.aliyuncs.com/api/ds-cars-service.json",
-    CARS: "http://localhost:8080/datasets/ds-cars-service.json",
+    CARS: "/datasets/ds-cars-service.json",
     // STUDENTS: "https://chspace.oss-cn-hongkong.aliyuncs.com/datasets/ds-students-service.json",
-    STUDENTS: "http://localhost:8080/datasets/ds-students-service.json",
-    BTC_GOLD: "http://localhost:8080/datasets/ds_btc_gold_service.json",
-    BIKE_SHARING: 'http://localhost:8080/datasets/ds-bikesharing-service.json',
-    CAR_SALES: 'http://localhost:8080/datasets/ds-carsales-service.json',
-    COLLAGE: 'http://localhost:8080/datasets/ds-collage-service.json',
-    TITANIC: 'http://localhost:8080/datasets/ds-titanic-service.json',
-    KELPER: 'http://localhost:8080/datasets/ds-kelper-service.json',
+    STUDENTS: "/datasets/ds-students-service.json",
+    BTC_GOLD: "/datasets/ds_btc_gold_service.json",
+    BIKE_SHARING: '/datasets/ds-bikesharing-service.json',
+    CAR_SALES: '/datasets/ds-carsales-service.json',
+    COLLAGE: '/datasets/ds-collage-service.json',
+    TITANIC: '/datasets/ds-titanic-service.json',
+    KELPER: '/datasets/ds-kelper-service.json',
 } as const;
 
 interface IPublicData {
