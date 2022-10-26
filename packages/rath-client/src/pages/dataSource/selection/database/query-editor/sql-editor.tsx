e17b2c915b82ee4a0ca6a4058d57ed182e194b45
qimeng <3263317228@qq.com>
2022-10-26 23:31:20 +0800

    添加sql编译器

--- packages/rath-client/src/pages/dataSource/selection/database/query-editor/sql-editor.tsx
+++ packages/rath-client/src/pages/dataSource/selection/database/query-editor/sql-editor.tsx
@@ -1,12 +1,46 @@
-import { memo } from "react";
-import { QueryEditorProps } from ".";
+import { memo, useEffect, useRef, useState } from 'react';
+import { QueryEditorProps } from '.';
+import { EditorView, basicSetup } from 'codemirror';
+import { sql } from '@codemirror/lang-sql';
+import { DefaultButton } from '@fluentui/react';
 
-
-const SQLEditor = memo<QueryEditorProps>(function QueryEditor ({  }) {
+const SQLEditor = memo<QueryEditorProps>(function QueryEditor({ query, tables, setQuery }) {
+    const container = useRef<HTMLDivElement>(null);
+    const [view, setView] = useState<any>(null);
+    useEffect(() => {
+        if (container.current) {
+            const editorView = new EditorView({
+                extensions: [basicSetup, sql()],
+                parent: container.current,
+            });
+            setView(editorView);
+        }
+    }, []);
     return (
-        <></>
+        <div style={{ width: '100%', height: 300, marginTop: 10 }}>
+            <div style={{ width: '100%', display: 'flex', justifyContent: 'flex-end', alignItems: 'center' }}>
+                <DefaultButton
+                    onClick={() => {
+                        if (view !== null) {
+                            console.log("2")
+                            setQuery(view.state.doc.text);
+                        }
+                    }}
+                >
+                    Run
+                </DefaultButton>
+            </div>
+            <div style={{ display: 'flex' }}>
+                <div style={{ flex: 1 }}>
+                    <div
+                        style={{ width: '100%', height: '200px', overflow: 'auto' }}
+                        ref={container}
+                        className="sqlEditor"
+                    ></div>
+                </div>
+            </div>
+        </div>
     );
 });
-
 
 export default SQLEditor;
