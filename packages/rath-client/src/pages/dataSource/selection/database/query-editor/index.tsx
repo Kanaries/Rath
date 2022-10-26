17464296476f2bda80cb6676bc9a88e2c439eaea
kyusho <antoineyang99@gmail.com>
2022-10-26 15:10:21 +0800

    feat(datasource): dataset editor use tabs

---
+++ packages/rath-client/src/pages/dataSource/selection/database/query-editor/index.tsx
@@ -0,0 +1,118 @@
+import { memo, useEffect, useMemo, useState } from "react";
+import styled from "styled-components";
+import DiagramEditor from "./diagram-editor";
+import SQLEditor from "./sql-editor";
+
+
+const Container = styled.div({
+    flexGrow: 1,
+    display: 'flex',
+    flexDirection: 'column',
+
+    '> *[role=tablist]': {
+        display: 'flex',
+        flexDirection: 'row',
+
+        '> *[role=tab]': {
+            border: '1px solid #888',
+            borderLeft: 'none',
+            userSelect: 'none',
+
+            '&:first-child': {
+                borderLeft: '1px solid #888',
+            },
+            '&[aria-selected=false]': {
+                cursor: 'pointer',
+            },
+            '&[aria-disabled=true]': {
+                opacity: 0.6,
+            },
+            '&[aria-selected=true]': {
+                borderBottomColor: 'transparent',
+                cursor: 'default',
+            },
+        },
+        '::after': {
+            content: '""',
+            display: 'block',
+            flexGrow: 1,
+            flexShrink: 1,
+            borderBottom: '1px solid #888',
+        },
+    },
+    '> *[role=tabpanel]': {},
+});
+
+export interface QueryEditorProps {
+    tables: string[] | 'input';
+    query: string;
+    setQuery: (query: string) => void;
+}
+
+export type QueryEditorMode = 'diagram' | 'query';
+
+const QueryEditor = memo<QueryEditorProps>(function QueryEditor ({ tables, query, setQuery }) {
+    const [mode, setMode] = useState<QueryEditorMode>('diagram');
+
+    const modes = useMemo<{
+        mode: QueryEditorMode;
+        enabled: boolean;
+    }[]>(() => {
+        return [
+            {
+                mode: 'diagram',
+                enabled: Array.isArray(tables),
+            },
+            {
+                mode: 'query',
+                enabled: true,
+            },
+        ];
+    }, []);
+
+    useEffect(() => {
+        if (tables === 'input' && mode === 'diagram') {
+            setMode('query');
+        }
+    }, [tables]);
+
+    return (
+        <Container>
+            <div role="tablist">
+                {modes.map(m => (
+                    <div
+                        role="tab"
+                        aria-selected={m.mode === mode}
+                        aria-disabled={!m.enabled}
+                        key={m.mode}
+                        onClick={() => {
+                            if (m.mode !== mode && m.enabled) {
+                                setMode(m.mode);
+                            }
+                        }}
+                    >
+                        {m.mode}
+                    </div>
+                ))}
+            </div>
+            <div role="tabpanel">
+                {mode === 'diagram' ? (
+                    <DiagramEditor
+                        tables={tables}
+                        query={query}
+                        setQuery={setQuery}
+                    />
+                ) : (
+                    <SQLEditor
+                        tables={tables}
+                        query={query}
+                        setQuery={setQuery}
+                    />
+                )}
+            </div>
+        </Container>
+    );
+});
+
+
+export default QueryEditor;
