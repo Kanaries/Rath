5f820aaa0acd318e1b33e68411236456f2b0a129
kyusho <antoineyang99@gmail.com>
2022-10-29 00:04:32 +0800

    fix(): outdated type ref

--- packages/rath-client/src/pages/dataSource/selection/database/query-editor/diagram-editor.tsx
+++ packages/rath-client/src/pages/dataSource/selection/database/query-editor/diagram-editor.tsx
@@ -5,20 +5,20 @@ import { toSQL } from "../../../../../components/dbGraph/utils";
 import { QueryEditorProps } from ".";
 
 
-const DiagramEditor = memo<QueryEditorProps>(function DiagramEditor ({ query, tables, setQuery, preview }) {
+const DiagramEditor = memo<QueryEditorProps>(function DiagramEditor ({ tableEnumerable, query, tables, setQuery, preview }) {
     const [graph, setGraph] = useState<IDBGraph>({
         nodes: [],
         edges: [],
     });
 
     useEffect(() => {
-        if (tables !== 'input') {
+        if (tableEnumerable) {
             const sql = toSQL(graph, tables);
             setQuery(sql);
         }
-    }, [graph, setQuery, tables]);
+    }, [graph, setQuery, tables, tableEnumerable]);
 
-    return tables === 'input' ? null : (
+    return !tableEnumerable ? null : (
         <DbGraph
             tables={tables}
             graph={graph}
