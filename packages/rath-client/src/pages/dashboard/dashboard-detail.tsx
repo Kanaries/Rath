4c7bd0d683e5e7f773e5dd62f07bd7f792d18d77
kyusho <antoineyang99@gmail.com>
2022-11-02 15:14:59 +0800

    feat(dashboard): divide renderer and editor

--- packages/rath-client/src/pages/dashboard/dashboard-detail.tsx
+++ packages/rath-client/src/pages/dashboard/dashboard-detail.tsx
@@ -1,10 +1,9 @@
 import { ActionButton } from '@fluentui/react';
 import { observer } from 'mobx-react-lite';
-import { FC, MouseEvent as MEvent, useCallback, useEffect, useRef, useState } from 'react';
+import { FC, useCallback, useState } from 'react';
 import styled from 'styled-components';
 import { useGlobalStore } from '../../store';
-import { DashboardCard } from '../../store/dashboardStore';
-import Card, { scaleRatio } from './renderer/card';
+import DashboardDraft from './dashboard-draft';
 
 
 const PageLayout = styled.div`
@@ -32,146 +31,20 @@ const Container = styled.div`
     overflow: auto;
 `;
 
-const Draft = styled.div<{ edit: boolean }>`
-    background-color: #fff;
-    border-radius: 2px;
-    margin: 16px;
-    box-shadow:
-        0 1.6px 3.6px 0 rgb(0 0 0 / 13%), 0 0.3px 0.9px 0 rgb(0 0 0 / 11%),
-        0 -1.6px 3.6px 0 rgb(0 0 0 / 13%), 0 -0.3px 0.9px 0 rgb(0 0 0 / 11%);
-    ${({ edit }) => edit ? `
-    background-image:
-        linear-gradient(to right, #8884 0.5px, transparent 0.5px),
-        linear-gradient(to left, #8884 0.5px, transparent 0.5px),
-        linear-gradient(to top, #8884 0.5px, transparent 0.5px),
-        linear-gradient(to bottom, #8884 0.5px, transparent 0.5px);
-    ` : ''}
-    background-repeat: repeat;
-    background-position: 0 0;
-    position: relative;
-    cursor: ${({ edit }) => edit ? 'crosshair' : 'default'};
-`;
-
-const DragBox = styled.div`
-    position: absolute;
-    border: 1px solid #888a;
-`;
-
 export interface DashboardDetailProps {
     cursor: number;
     /** back to dashboard list */
     goBack: () => void;
 }
 
-const transformCoord = (target: HTMLElement, ev: { clientX: number; clientY: number }, w: number, h: number) => {
-    const { x, y } = target.getBoundingClientRect();
-
-    return {
-        x: Math.max(0, Math.min(Math.round((ev.clientX - x) / scaleRatio), w)),
-        y: Math.max(0, Math.min(Math.round((ev.clientY - y) / scaleRatio), h)),
-    };
-};
-
-const checkOverlap = (a: DashboardCard['layout'], b: DashboardCard['layout']): boolean => {
-    const x1 = a.x;
-    const x2 = a.x + a.w;
-    const y1 = a.y;
-    const y2 = a.y + a.h;
-    for (const [x, y] of [[x1, y1], [x1, y2], [x2, y1], [x2, y2]]) {
-        if (b.x < x && b.x + b.w > x && b.y < y && b.y + b.h > y) {
-            return true;
-        }
-    }
-    return false;
-};
-
-const MIN_CARD_SIZE = 64;
-
 const DashboardDetail: FC<DashboardDetailProps> = ({ cursor, goBack }) => {
     const { dashboardStore } = useGlobalStore();
-    const page = dashboardStore.pages[cursor];
-    const { operators: { addCard } } = dashboardStore.fromPage(cursor);
 
     const [mode, setMode] = useState<'edit' | 'preview'>('preview');
 
     const toggleMode = useCallback(() => {
         setMode(mode === 'edit' ? 'preview' : 'edit');
     }, [mode]);
-
-    const draftRef = useRef<HTMLDivElement>(null);
-
-    const [dragging, setDragging] = useState<null | { from: { x: number; y: number }; to: { x: number; y: number } }>(null);
-
-    const handleDragEnd = useCallback((pos: { x: number; y: number }) => {
-        if (!dragging) {
-            return;
-        }
-        setDragging(null);
-        const w = Math.abs(dragging.from.x - pos.x);
-        const h = Math.abs(dragging.from.y - pos.y);
-        if (w >= MIN_CARD_SIZE && h >= MIN_CARD_SIZE) {
-            const layout = {
-                x: Math.min(dragging.from.x, pos.x),
-                y: Math.min(dragging.from.y, pos.y),
-                w,
-                h,
-            };
-            // 判断重叠
-            for (const card of page.cards) {
-                if (checkOverlap(card.layout, layout)) {
-                    return;
-                }
-            }
-            addCard(layout);
-        }
-    }, [dragging, addCard, page.cards]);
-
-    const handleDragCancel = useCallback(() => {
-        setDragging(null);
-    }, [dragging]);
-
-    useEffect(() => {
-        if (dragging) {
-            const dragEnd = (ev: MouseEvent) => {
-                if (ev.type === 'mouseup' && draftRef.current) {
-                    const { x, y } = transformCoord(draftRef.current, ev, page.config.size.w, page.config.size.h);
-                    handleDragEnd({ x, y });
-                } else if (ev.type === 'mousemove' && ev.buttons !== 1) {
-                    handleDragCancel();
-                }
-            };
-            document.body.addEventListener('mouseup', dragEnd);
-            document.body.addEventListener('mousemove', dragEnd);
-            return () => {
-                document.body.removeEventListener('mouseup', dragEnd);
-                document.body.removeEventListener('mousemove', dragEnd);
-            };
-        }
-    }, [dragging, handleDragCancel, page.config.size]);
-
-    const handleMouseDown = useCallback((ev: MEvent<HTMLDivElement>) => {
-        if (mode === 'preview' || !draftRef.current) {
-            return;
-        }
-        const { x, y } = transformCoord(draftRef.current, ev, page.config.size.w, page.config.size.h);
-        setDragging({
-            from: { x, y },
-            to: { x, y },
-        });
-    }, [mode, handleDragEnd, page.config.size]);
-
-    const handleMouseMove = useCallback((ev: MEvent<HTMLDivElement>) => {
-        if (mode === 'preview') {
-            return;
-        }
-        if (dragging && draftRef.current) {
-            const { x, y } = transformCoord(draftRef.current, ev, page.config.size.w, page.config.size.h);
-            setDragging({
-                from: dragging.from,
-                to: { x, y },
-            });
-        }
-    }, [mode, dragging, page.config.size]);
 
     return (
         <PageLayout>
@@ -180,35 +53,7 @@ const DashboardDetail: FC<DashboardDetailProps> = ({ cursor, goBack }) => {
                 <ActionButton iconProps={{ iconName: mode === 'edit' ? 'AnalyticsView' : 'Edit' }} onClick={toggleMode} />
             </Header>
             <Container>
-                <Draft
-                    ref={draftRef}
-                    edit={mode === 'edit'}
-                    style={{
-                        width: `${page.config.size.w * scaleRatio}px`,
-                        height: `${page.config.size.h * scaleRatio}px`,
-                        backgroundSize: new Array<0>(4).fill(0).map(() => `${scaleRatio}px ${scaleRatio}px`).join(','),
-                    }}
-                    onMouseDown={handleMouseDown}
-                    onMouseMove={handleMouseMove}
-                >
-                    {page.cards.map((card, i) => (
-                        <Card
-                            key={i}
-                            card={card}
-                            readOnly={mode === 'preview'}
-                        />
-                    ))}
-                    {dragging && (
-                        <DragBox
-                            style={{
-                                left: Math.min(dragging.from.x, dragging.to.x) * scaleRatio,
-                                top: Math.min(dragging.from.y, dragging.to.y) * scaleRatio,
-                                width: Math.abs(dragging.from.x - dragging.to.x) * scaleRatio,
-                                height: Math.abs(dragging.from.y - dragging.to.y) * scaleRatio,
-                            }}
-                        />
-                    )}
-                </Draft>
+                <DashboardDraft cursor={cursor} mode={mode} />
             </Container>
         </PageLayout>
     );
--- packages/rath-client/src/pages/dashboard/renderer/card.tsx
+++ packages/rath-client/src/pages/dashboard/renderer/card.tsx
@@ -1,15 +1,38 @@
 import { observer } from "mobx-react-lite";
-import type { FC } from "react";
+import { FC, useMemo } from "react";
 import styled from "styled-components";
-import type { DashboardCard } from "../../../store/dashboardStore";
-
-
-export const scaleRatio = Math.min(window.innerWidth, window.innerHeight) / 256;
+import type { DashboardCard, DashboardDocumentOperators } from "../../../store/dashboardStore";
+import { scaleRatio } from "../dashboard-draft";
+import CardDisplay from "./card-display";
+import CardEditor from "./card-editor";
 
 export interface CardProps {
     card: DashboardCard;
-    /** @default false */
-    readOnly?: boolean;
+    index: number;
+    /** @default undefined */
+    editor?: Omit<CardProviderProps, 'children' | 'transformCoord' | 'item' | 'index'> | undefined;
+    transformCoord: (ev: { clientX: number; clientY: number }) => { x: number; y: number };
+}
+
+export interface CardProvider {
+    content: JSX.Element | null;
+    onMouseEnter: () => void;
+    onMouseLeave: () => void;
+    onRootMouseDown: (x: number, y: number) => void;
+    onDoubleClick: () => void;
+}
+
+export interface CardProviderProps {
+    children: (provider: Partial<CardProvider>) => JSX.Element;
+    transformCoord: (ev: { clientX: number; clientY: number }) => { x: number; y: number };
+    draftRef: { current: HTMLElement | null };
+    index: number;
+    item: Readonly<DashboardCard>;
+    canDrop: (layout: DashboardCard['layout'], except?: number) => boolean;
+    isSizeValid: (w: number, h: number) => boolean;
+    operators: Partial<DashboardDocumentOperators & {
+        adjustCardSize: (dir: 'n' | 'e' | 's' | 'w') => void;
+    }>;
 }
 
 const CardBox = styled.div`
@@ -18,22 +41,47 @@ const CardBox = styled.div`
     position: absolute;
     border: 1px solid #8888;
     backdrop-filter: blur(1px);
+    cursor: default;
 `;
 
-const Card: FC<CardProps> = ({ card, readOnly = false }) => {
+const Card: FC<CardProps> = ({ card, editor, transformCoord, index }) => {
+    const Provider = useMemo(() => {
+        return editor ? CardEditor : CardDisplay;
+    }, [editor]);
+
     return (
-        <CardBox
-            onMouseDown={e => e.stopPropagation()}
-            style={{
-                left: card.layout.x * scaleRatio,
-                top: card.layout.y * scaleRatio,
-                width: card.layout.w * scaleRatio,
-                height: card.layout.h * scaleRatio,
-                cursor: readOnly ? 'default' : 'pointer',
+        <Provider
+            {...editor ?? {
+                draftRef: { current: null },
+                canDrop: () => false,
+                isSizeValid: () => false,
+                operators: {},
             }}
+            transformCoord={transformCoord}
+            item={card}
+            index={index}
         >
-            我是 placeholder
-        </CardBox>
+            {provider => (
+                <CardBox
+                    onMouseEnter={provider.onMouseEnter}
+                    onMouseLeave={provider.onMouseLeave}
+                    onMouseDown={e => {
+                        e.stopPropagation();
+                        const pos = transformCoord(e);
+                        provider.onRootMouseDown?.(pos.x, pos.y);
+                    }}
+                    onDoubleClick={provider.onDoubleClick}
+                    style={{
+                        left: card.layout.x * scaleRatio,
+                        top: card.layout.y * scaleRatio,
+                        width: card.layout.w * scaleRatio,
+                        height: card.layout.h * scaleRatio,
+                    }}
+                >
+                    {provider.content}
+                </CardBox>
+            )}
+        </Provider>
     );
 };
 
