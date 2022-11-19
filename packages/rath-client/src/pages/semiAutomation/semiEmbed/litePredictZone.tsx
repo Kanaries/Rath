54ea9d81c7e1b8dd167f762f1816b94037f2a50f
jojocys <270001151@qq.com>
2022-11-19 17:26:58 +0800

    feat: embed semi support neighbors

--- packages/rath-client/src/pages/semiAutomation/semiEmbed/litePredictZone.tsx
+++ packages/rath-client/src/pages/semiAutomation/semiEmbed/litePredictZone.tsx
@@ -1,9 +1,11 @@
 import React, { useState } from 'react';
 import { Pivot, PivotItem } from '@fluentui/react';
+import intl from 'react-intl-universal';
 import PattSegment from '../predictZone/pattSegment';
 import FeatSegment from '../predictZone/featSegment';
 import FilterSegment from '../predictZone/filterSegment';
 import { IRenderViewKey } from '../../../store/semiAutomation/localTypes';
+import NeighborSegment from '../predictZone/neighborSegment';
 
 const LitePredictZone: React.FC = (props) => {
     const [viewType, setViewType] = useState<IRenderViewKey>('pattViews');
@@ -14,14 +16,16 @@ const LitePredictZone: React.FC = (props) => {
                     item && setViewType(item.props.itemKey as IRenderViewKey);
                 }}
             >
-                <PivotItem headerText="Patterns" itemKey="pattViews" />
-                <PivotItem headerText="Features" itemKey="featViews" />
-                <PivotItem headerText="Subsets" itemKey="filterViews" />
+                <PivotItem headerText={intl.get('semiAuto.main.associateShorthand.neighbors')} itemKey="neighborViews" />
+                <PivotItem headerText={intl.get('semiAuto.main.associateShorthand.patterns')} itemKey="pattViews" />
+                <PivotItem headerText={intl.get('semiAuto.main.associateShorthand.features')} itemKey="featViews" />
+                <PivotItem headerText={intl.get('semiAuto.main.associateShorthand.filters')} itemKey="filterViews" />
             </Pivot>
             <div>
                 {viewType === 'pattViews' && <PattSegment />}
                 {viewType === 'featViews' && <FeatSegment />}
                 {viewType === 'filterViews' && <FilterSegment />}
+                {viewType === 'neighborViews' && <NeighborSegment />}
             </div>
         </div>
     );
