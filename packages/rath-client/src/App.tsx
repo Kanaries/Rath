42fb00b94bf93fdb98db3756c5c68681904176f2
kyusho <antoineyang99@gmail.com>
2022-11-23 20:57:24 +0800

    fix(user): unexpected avatar panel

--- packages/rath-client/src/App.tsx
+++ packages/rath-client/src/App.tsx
@@ -41,7 +41,7 @@ export interface PreferencesListType {
 const preferencesList: PreferencesListType[] = [
     { key: PreferencesType.Account, name: PreferencesType.Account, icon: 'Home', element: () => <Account /> },
     // { key: PreferencesType.Info, name: PreferencesType.Info, icon: 'Info', element: () => <Info /> },
-    { key: PreferencesType.Header, name: PreferencesType.Header, icon: 'Contact', element: () => <Header /> },
+    // { key: PreferencesType.Header, name: PreferencesType.Header, icon: 'Contact', element: () => <Header /> },
     { key: PreferencesType.Setting, name: PreferencesType.Setting, icon: 'Settings', element: () => <Setup /> },
 ];
 
