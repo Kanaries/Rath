500dd9d7c85dd0bef126f2ca7034ae1a96d8d7be
qimeng <3263317228@qq.com>
2022-11-23 14:47:09 +0800

    fix:手机号正则变更

--- packages/rath-client/src/pages/loginInfo/access/valueCheck.ts
+++ packages/rath-client/src/pages/loginInfo/access/valueCheck.ts
@@ -21,5 +21,6 @@ export function validEduMail(val: string): boolean {
 export function validPhone(val: string, area: string) {
     // const PhoneRegex = /^(13[0-9]|14[01456879]|15[0-35-9]|16[2567]|17[0-8]|18[0-9]|19[0-35-9])\d{8}$/;
     // return area === 'zh-CN' ? PhoneRegex.test(val) : val.length > 0;
-    return val.length !== 0;
+    const result = isNaN(Number(val));
+    return val.length !== 0 && !result;
 }
