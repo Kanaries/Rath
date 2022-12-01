782ccc27d3a180805e6bd19a98ac09a718162d92
dev02 <kanaries_i.dev02@outlook.com>
2022-12-01 16:30:19 +0800

    build

--- services/causal-service/run-docker.sh
+++ services/causal-service/run-docker.sh
@@ -3,7 +3,7 @@ PORT=8001
 PORT2=2281
 cur_dir=`dirname $0`
 cd $cur_dir
-. fetch.sh && \
+. $cur_dir/fetch.sh && \
 docker build -t causal-server . && \
-docker run -d -p $PORT:8000 -p $PORT2:8000 --name run-causal-server causal-server
+docker run -d --rm -p $PORT:8000 -p $PORT2:8000 --name run-causal-server causal-server
 # docker run -it --rm -v $(pwd):/app  -p $PORT:8000 -p $PORT2:8000 --name run-causal-server causal-server
