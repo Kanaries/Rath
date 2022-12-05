82f26c158b0010cdc83d4b7f0a9c78dc903a0fbe
jojocys <yuyilobay@gmail.com>
2022-12-05 20:09:30 +0800

    feat: zip files

--- packages/rath-client/src/utils/download.ts
+++ packages/rath-client/src/utils/download.ts
@@ -8,3 +8,32 @@ export function downloadFileWithContent(content: string, fileName: string) {
 
     document.body.removeChild(ele);
 }
+
+export function downloadFileFromBlob(content: Blob, fileName: string) {
+    const ele = document.createElement('a');
+    ele.setAttribute('href', URL.createObjectURL(content));
+    ele.setAttribute('download', fileName);
+    ele.style.display = 'none';
+    document.body.appendChild(ele);
+    ele.click();
+
+    document.body.removeChild(ele);
+}
+
+export enum IKRFComponents {
+    data = 'data',
+    meta = 'meta',
+    collection = 'collection',
+    causal = 'causal',
+    dashboard = 'dashboard',
+    mega = 'mega',
+}
+export function getKRFParseMap (props: { [key in IKRFComponents]: boolean }): { [key in IKRFComponents]?: string } {
+    const parseMap: { [key in IKRFComponents]?: string } = {};
+    Object.keys(props).forEach((key) => {
+        if (props[key as IKRFComponents]) {
+            parseMap[key as IKRFComponents] = `rath_${IKRFComponents[key as IKRFComponents]}.json`;
+        }
+    });
+    return parseMap
+}
