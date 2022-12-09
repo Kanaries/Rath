2bb07788b07ba665e95ccb0bac1646f07444a194
kyusho <antoineyang99@gmail.com>
2022-12-09 21:28:12 +0800

    feat(causal): use message-bar to display no-connection information

--- packages/rath-client/src/pages/causal/noConnection.tsx
+++ packages/rath-client/src/pages/causal/noConnection.tsx
@@ -1,17 +1,17 @@
 import intl from 'react-intl-universal';
 import type { FC } from "react";
 import styled from 'styled-components';
-import { Icon } from '@fluentui/react';
+import { Icon, MessageBar, MessageBarType } from '@fluentui/react';
 
 
 const Container = styled.p`
     color: #ff8c00;
     font-size: 0.8rem;
     margin: 1em 0;
-    > i {
+    & i {
         margin: 0 0.4em;
     }
-    > a {
+    & a {
         margin: 0 0.2em;
         color: #0078d4;
         text-decoration: underline;
@@ -31,14 +31,19 @@ const SupportMailAddr = 'support@kanaries.org';
 const NoConnection: FC = () => {
     return (
         <Container>
-            <Icon iconName="BlockedSite" />
-            <span>{intl.get('causal.no_connection.before')}</span>
-            <a href={`mailto:${SupportMailAddr}?subject=${encodeURIComponent(intl.get('causal.no_connection.subject'))}`}>
-                {intl.get('causal.no_connection.text')}
-                {`(${SupportMailAddr})`}
-                <Icon iconName="MailForward" />
-            </a>
-            <span>{intl.get('causal.no_connection.after')}</span>
+            <MessageBar
+                isMultiline={false}
+                messageBarType={MessageBarType.warning}
+                styles={{ icon: { paddingTop: '0.16em' } }}
+            >
+                <span>{intl.get('causal.no_connection.before')}</span>
+                <a href={`mailto:${SupportMailAddr}?subject=${encodeURIComponent(intl.get('causal.no_connection.subject'))}`}>
+                    {intl.get('causal.no_connection.text')}
+                    {`(${SupportMailAddr})`}
+                    <Icon iconName="MailForward" />
+                </a>
+                <span>{intl.get('causal.no_connection.after')}</span>
+            </MessageBar>
         </Container>
     );
 };
