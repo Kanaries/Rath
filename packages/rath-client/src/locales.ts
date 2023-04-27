f42620d63b154c056a54b66a1aad74affeacc0b5
k6sdevbob <dev.bob@kanaries.net>
2023-04-27 16:03:36 +0800

    feat(i18n): support ja & ko
    Co-Authored-By: kyusho <antoineyang99@gmail.com>
    (cherry picked from commit 3a3404711dadf9b77a61b7b911ea545e8aa7a119)

--- packages/rath-client/src/locales.ts
+++ packages/rath-client/src/locales.ts
@@ -6,5 +6,13 @@ export const SUPPORT_LANG = [
     {
         name: '中文(Chinese)',
         value: 'zh-CN'
-    }
+    },
+    {
+        name: '日本語(Japanese)',
+        value: 'ja-JP'
+    },
+    {
+        name: '한국어(Korean)',
+        value: 'ko-KR'
+    },
 ]
