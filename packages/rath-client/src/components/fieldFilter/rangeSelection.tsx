6493896d2c8d07664717b275cb8781e41ef9e57c
AntoineYANG <dev.bob@kanaries.net>
2023-06-08 11:11:59 +0800

    fix(filter): datetime slider ui bug
    Co-Authored-By: kyusho <antoineyang99@gmail.com>

--- packages/rath-client/src/components/fieldFilter/rangeSelection.tsx
+++ packages/rath-client/src/components/fieldFilter/rangeSelection.tsx
@@ -1,3 +1,4 @@
+import intl from 'react-intl-universal';
 import { Slider } from '@fluentui/react';
 import React, { useCallback } from 'react';
 
@@ -8,12 +9,30 @@ interface RangeSelectionProps {
     onValueChange: (range: [number, number]) => void;
     type: 'number' | 'time';
 }
+
+const DateTimeValueLabelStyle = {
+    // monospace
+    fontFamily: 'Courier New',
+    // narrowed
+    letterSpacing: '-0.05em',
+    transform: 'scaleX(0.95)',
+    marginInline: '-1%',
+} as const;
+
 const RangeSelection: React.FC<RangeSelectionProps> = (props) => {
     const { range, left, right, onValueChange, type } = props;
 
     const formatter = useCallback((v: number) => {
         if (type === 'time') {
-            return new Date(v).toLocaleString();
+            return intl.get('date_format', {
+                Y: `${new Date(v).getFullYear()}`.padStart(4, ' '),
+                m: intl.get(`time_format.shortMonths.${new Date(v).getDay()}`),
+                d: `${new Date(v).getDate()}`.padStart(2, '0'),
+                w: intl.get(`time_format.shortDays.${new Date(v).getDay()}`),
+                H: `${new Date(v).getHours()}`.padStart(2, '0'),
+                M: `${new Date(v).getMinutes()}`.padStart(2, '0'),
+                S: `${new Date(v).getSeconds()}`.padStart(2, '0'),
+            });
         }
         return `${v}`;
     }, [type]);
@@ -42,10 +61,12 @@ const RangeSelection: React.FC<RangeSelectionProps> = (props) => {
                 },
                 slideBox: {
                     flex: 1,
+                    minWidth: '120px',
                 },
                 valueLabel: {
                     minWidth: '40px',
                     width: 'unset',
+                    ...(type === 'time' ? DateTimeValueLabelStyle : {}),
                 },
             }}
         />
