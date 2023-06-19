218fbce5760c29d8eac092e800fe12386cd715d4
AntoineYANG <dev.bob@kanaries.net>
2023-06-19 21:11:41 +0800

    refactor(datasource): remove unused files
    Co-Authored-By: kyusho <antoineyang99@gmail.com>

--- packages/rath-client/src/pages/dataConnection/database/query-editor/interfaces.ts
+++
@@ -1,9 +0,0 @@
-import { TableInfo } from '../api';
-
-export interface QueryEditorProps {
-    tableEnumerable: boolean;
-    tables: TableInfo[];
-    query: string;
-    setQuery: (query: string) => void;
-    preview: (query?: string) => void;
-}
