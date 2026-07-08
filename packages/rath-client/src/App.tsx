679518c70597f951dc94d042bfd90814afae1d75
Elwynn Chen <ooer@kanaries.net>
2026-07-08 03:15:15 -0700

    perf(client): lazy load app pages

--- packages/rath-client/src/App.tsx
+++ packages/rath-client/src/App.tsx
@@ -1,26 +1,26 @@
-import { useEffect, useState } from 'react';
+import { Suspense, lazy, useEffect, useState } from 'react';
 import { observer } from 'mobx-react-lite';
 import { Spinner, SpinnerSize } from '@fluentui/react';
 import './normalize.css';
 import './App.css';
 import { useGlobalStore, StoreWrapper } from './store/index';
-import VisualInterface from './pages/manualControl';
-import DataSourceBoard from './pages/dataSource/index';
-import PatternPage from './pages/semiAutomation/index';
-import LTSPage from './pages/megaAutomation';
 import AppNav from './components/appNav';
 import { destroyRathWorker, initRathWorker } from './services/index';
 import { PIVOT_KEYS } from './constants';
 import CrInfo from './components/crInfo';
-import ProgressiveDashboard from './pages/progressiveDashboard';
-import Painter from './pages/painter';
-import Collection from './pages/collection';
-import Dashboard from './pages/dashboard';
-import CausalPage from './pages/causal';
 import PerformanceWindow from './components/performance-window';
 import useHotKey from './hooks/use-hotkey';
-import DataConnection from './pages/dataConnection';
 
+const VisualInterface = lazy(() => import('./pages/manualControl'));
+const DataSourceBoard = lazy(() => import('./pages/dataSource/index'));
+const PatternPage = lazy(() => import('./pages/semiAutomation/index'));
+const LTSPage = lazy(() => import('./pages/megaAutomation'));
+const ProgressiveDashboard = lazy(() => import('./pages/progressiveDashboard'));
+const Painter = lazy(() => import('./pages/painter'));
+const Collection = lazy(() => import('./pages/collection'));
+const Dashboard = lazy(() => import('./pages/dashboard'));
+const CausalPage = lazy(() => import('./pages/causal'));
+const DataConnection = lazy(() => import('./pages/dataConnection'));
 
 function App() {
     const { langStore, commonStore } = useGlobalStore();
@@ -53,16 +53,18 @@ function App() {
                     <AppNav />
                 </div>
                 <div className="main-app-content">
-                    {appKey === PIVOT_KEYS.dataSource && <DataSourceBoard />}
-                    {appKey === PIVOT_KEYS.editor && <VisualInterface />}
-                    {appKey === PIVOT_KEYS.megaAuto && <LTSPage />}
-                    {appKey === PIVOT_KEYS.semiAuto && <PatternPage />}
-                    {appKey === PIVOT_KEYS.painter && <Painter />}
-                    {appKey === PIVOT_KEYS.dashBoardDesigner && <ProgressiveDashboard />}
-                    {appKey === PIVOT_KEYS.collection && <Collection />}
-                    {appKey === PIVOT_KEYS.dashboard && <Dashboard />}
-                    {appKey === PIVOT_KEYS.causal && <CausalPage />}
-                    {appKey === PIVOT_KEYS.connection && <DataConnection />}
+                    <Suspense fallback={<Spinner label="Loading..." size={SpinnerSize.large} />}>
+                        {appKey === PIVOT_KEYS.dataSource && <DataSourceBoard />}
+                        {appKey === PIVOT_KEYS.editor && <VisualInterface />}
+                        {appKey === PIVOT_KEYS.megaAuto && <LTSPage />}
+                        {appKey === PIVOT_KEYS.semiAuto && <PatternPage />}
+                        {appKey === PIVOT_KEYS.painter && <Painter />}
+                        {appKey === PIVOT_KEYS.dashBoardDesigner && <ProgressiveDashboard />}
+                        {appKey === PIVOT_KEYS.collection && <Collection />}
+                        {appKey === PIVOT_KEYS.dashboard && <Dashboard />}
+                        {appKey === PIVOT_KEYS.causal && <CausalPage />}
+                        {appKey === PIVOT_KEYS.connection && <DataConnection />}
+                    </Suspense>
                     <CrInfo />
                 </div>
             </div>
